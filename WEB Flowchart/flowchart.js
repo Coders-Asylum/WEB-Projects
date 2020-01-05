0d78354bdc34fdf15f29ace6cd7b00a4a1e6780e
MAVERICK 099 <32545664+Maverick099@users.noreply.github.com>
2020-01-05 13:02:15 +0530

    Update flowchart.js

--- WEB Flowchart/flowchart.js
+++ WEB Flowchart/flowchart.js
@@ -1,1 +1,6 @@
+var decision1= document.getElementsByClassName(decisio1)
+var decision1_positionInfo=decision1.getBoundingClientRect();
+var decisio1_height=decision1_positionInfo.height;
+var decisio1_width=decision1_positionInfo.width;
 
+$(".decisio1").css("background-size", decisio1_width+"px " + decisio1_height + "px");
